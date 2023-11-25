7b7b1da5a141c4aa6251b6b70e264ad05c13101b
Pinguina-Git <151978028+Pinguina-Git@users.noreply.github.com>
2023-11-25 15:32:02 +1300

    initial commit
    Moved the weather app code to github

---
+++ src/script.js
@@ -0,0 +1,19 @@
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+console.log(months);
